6eb96997b7a9a685f952869f766f82067e92c60e
David Giroux <DavidGiroux149@gmail.com>
2023-12-30 16:26:23 -0500

    add error state handling to failed api fetch

--- src/client/components/LocalAPI.jsx
+++ src/client/components/LocalAPI.jsx
@@ -3,13 +3,19 @@ import axios from 'axios'
 
 const LocalApi = () => {
   const [text, setText] = useState([]);
+  const [error, setError] = useState(null);
 
   const url = "http://localhost:3000/local";
 
   const fetchData = () => {
     axios.get(url).then((res) => {
-      console.log(res.data.local_results);
+      console.log("data", res.data.local_results);
       setText(res.data.local_results);
+      setError(null);
+    }).catch(err => {
+      // We should handle the error and set the error state
+      setError('Failed to fetch data.');
+      console.error('API fetch error:', err);
     });
   };
 
@@ -21,6 +27,7 @@ const LocalApi = () => {
     <>
       <h1>App</h1>
       <button onClick={handleClick}>Click me to fetch data</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       {text.map((data) => (
         <div key={data.position}>
           <img src={data.thumbnail} alt="" />
