adad01d363dd954fdd5c980c71d8c3db580ca2e0
David Giroux <DavidGiroux149@gmail.com>
2023-12-30 16:27:11 -0500

    refactor main.js backend file

--- src/server/main.js
+++ src/server/main.js
@@ -1,28 +1,44 @@
 import express from "express";
-import ViteExpress from "vite-express";
-import axios from "axios";
+import { createServer as createViteServer } from 'vite';
+import apiRoutes from './routes/api.js'; 
 import 'dotenv/config';
 
-const app = express();
+async function createServer() {
+  
+  const app = express();
+  
+  // This is so we can parse JSON payloads:
+  app.use(express.json());
+  
+  // API routes will be used below:
+  app.use('./routes/api', apiRoutes);
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
-
-app.use(express.json());
-
-app.get('/local', async (req, res) => {
-  try {
-    const query = 'Beaches'
-    const location = 'Vancouver'
-    const apiUrl = `https://serpapi.com/search.json?engine=google_local&q=${query}&location=${location}&google_domain=google.com&api_key=${process.env.API_KEY}`; 
-    const response = await axios.get(apiUrl);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
+  // Only use Vite in development:
+  if (process.env.NODE_ENV !== 'production') {
+    // This creates a Vite server in middleware mode.
+    // The 'html' mode is designed to serve the index.html on all non-file requests,
+    // which is useful for Single Page Applications that perform client-side routing.
+    const vite = await createViteServer({
+      server: { middlewareMode: 'html' }
+    });
+    // The Vite server middlewares are added to the Express application. This means
+    // that when we run our server in development, Vite will handle requests for
+    // assets and serve them with HMR (Hot Module Replacement) and other development features.
+    app.use(vite.middlewares);
+  } else {
+    // In production, instead of using Vite, you serve the static files directly.
+    app.use(express.static('dist'));
   }
-});
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
-);
+  // It will fetch PORT info from our .env file or default to 3000
+  // This is helpful for development phase and if this goes on production
+  const PORT = process.env.PORT || 3000;
+
+  // Interpolation would avoid us to hardcode the port which is helpful for post-development:
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
+  });
+}
+
+createServer();
+
