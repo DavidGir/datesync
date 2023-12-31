b960d0a48bacd6349bd82475751f890a0ff9b751
David Giroux <DavidGiroux149@gmail.com>
2023-12-31 17:21:28 -0500

    add isLoading and error states with auth0 as well as components for authentication

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,21 +5,32 @@ import "./App.css";
 import CreateDate from "./routes/CreateDate";
 import Dashboard from "./routes/Dashboard";
 import LandingPage from "./routes/LandingPage";
-
+import LoginButton from './components/auth/LoginButton';
+import LogoutButton from './components/auth/LogoutButton';
+import Profile from './components/auth/Profile';
 
 function App() {
-  
-  
-  
-  
+  const {isLoading, error } = useAuth0();
+
   return (
-    <>
+    <main className="column">
+      <h1> Auth0 Login </h1>
+      {error && <p>Authentication Error</p>}
+      {/* If no error and is in isLoading state Loading will display; this is a perfect place to add a loading spinner or something */}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='/create-date' element={<CreateDate />} />
       <Route path= '/dashboard' element={<Dashboard />} />
     </Routes>
-    </>
+    </main>
   )
 }
 
