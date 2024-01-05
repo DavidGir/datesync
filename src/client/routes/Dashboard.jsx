73762b832faf2038b1a1564b359b47d5c654aead
David Giroux <DavidGiroux149@gmail.com>
2024-01-05 13:41:26 -0500

    add dashboard component logic to fetch backend protected api

--- src/client/routes/Dashboard.jsx
+++ src/client/routes/Dashboard.jsx
@@ -1,7 +1,31 @@
+import axios from "axios";
+import useAuthToken from "../hooks/useAuthToken"; 
+
 const Dashboard = () => {
+  const fetchAccessToken = useAuthToken();
+
+  const fetchData = async () => {
+    try {
+      // const token = await fetchAccessToken();
+
+      const hardcodedToken = import.meta.env.VITE_AUTH0_TEST_TOKEN;
+
+      const response = await axios.get('http://localhost:3000/protected/dashboard', {
+        headers: {
+          Authorization: `Bearer ${hardcodedToken}`
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching protected data:', error);
+    }
+  };
+
   return (
-    <h1>Hello from Dashboard!</h1>
-  )
-}
+    <div>
+      <button onClick={fetchData}>Load Data</button>
+    </div>
+  );
+};
 
 export default Dashboard;
